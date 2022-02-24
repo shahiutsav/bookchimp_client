02f61c9d9639342fbe2c3810cbe570c8201cabab
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2022-02-24 15:12:38 +0545

    The client can now access api

--- src/components/Books/BookCard/BookCard.js
+++ src/components/Books/BookCard/BookCard.js
@@ -2,24 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 
-const options = {
-  edit: false,
-  color: "rgba(20, 20, 20, 0.1",
-  activeColor: "tomato",
-  value: 3.5,
-  isHalf: true,
-  size: window.innerWidth < 600 ? 20 : 25,
-};
-
 const BookCard = ({ book }) => {
+  const options = {
+    edit: false,
+    color: "rgba(20, 20, 20, 0.1",
+    activeColor: "tomato",
+    value: book.ratings,
+    isHalf: true,
+    size: window.innerWidth < 600 ? 20 : 25,
+  };
+
   return (
     <Link className="col-4" to={book._id}>
       <img src={book.images[0].url} alt="" />
       <h4>{book.name}</h4>
-      <div>
+      <div class="review-section">
         <ReactStars {...options} />
+        <span>({book.numOfReviews} Reviews)</span>
       </div>
-      <p>{book.price} </p>
+      <p>{`$ ${book.price}`} </p>
     </Link>
   );
 };
