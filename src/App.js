0dcc5764641811f79f4e16bd72726871827c7f7d
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2022-02-24 10:41:39 +0545

    Created Book cards for the home menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar/Navbar";
 import Footer from "./components/layout/Footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import Book from "./components/layout/Books/Books.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Book />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
